d68592591f16d38252d657b45d45e6879498fd8c
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2021-01-31 18:15:47 +0000

    Quicksave

---
+++ Lab Sheet 1/Q1/Band.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    public class Band
+    {
+        public string BandName { get; set; }
+
+        public int YearFormed { get; set; }
+
+        public string Members { get; set; }
+
+        public Band(string bandName, int yearFormed, string members)
+        {
+            BandName = bandName;
+            YearFormed = yearFormed;
+            Members = members;
+        }
+
+        public Band()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return BandName;
+        }
+    }
+}
--- Lab Sheet 1/Q1/MainWindow.xaml.cs
+++ Lab Sheet 1/Q1/MainWindow.xaml.cs
@@ -24,5 +24,17 @@ namespace Q1
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Band b1 = new Band() { BandName = "The Foo Fighters", YearFormed = 1994, Members = "Dave Grohl, Nate Mendel, Pat Smear, Taylor Hawkins, Chris Shifflett, Rami Jafee" };
+            Band b2 = new Band() { BandName = "The Beatles", YearFormed = 1960 , Members = "John Lennon, Paul McCartney, George Harrison, Ringo Starr" };
+
+            Band b3 = new Band() { BandName = "The Rolling Stones", YearFormed = 1962, Members = "Mick Jagger, Ian Stewart, Dick Taylor, Bill Wymann, Mick Taylor" };
+            Band b4 = new Band() { BandName = "Green Day", YearFormed = 1986, Members = "Billie Joe Armstrong, Mike Dirnt, Tre Cool" };
+
+            Band b5 = new Band() { BandName = "Artic Monkeys", YearFormed = 2002, Members = "Alex Turner, Matt Heldens, Jamie Cook, Nick O'Malley" };
+            Band b6 = new Band() { BandName = "The Strokes", YearFormed = 1998, Members = "Julien Casablancas, Nick Valensi, Albert Hammond Jr, Nikolai Frature, Fabrizio Moretti" };
+        }
     }
 }
