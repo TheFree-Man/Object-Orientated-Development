cf3fa21fc1dba371d156599ce213da712f65afd5
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2021-02-01 23:05:09 +0000

    Quicksave

--- Lab Sheet 1/Q1/Band.cs
+++ Lab Sheet 1/Q1/Band.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Q1
 {
-    public class Band
+    public abstract class Band : IComparable
     {
         public string BandName { get; set; }
 
@@ -29,6 +29,36 @@ namespace Q1
         public override string ToString()
         {
             return BandName;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Band otherBand = obj as Band;
+            return this.BandName.CompareTo(otherBand.BandName);
+        }
+    }
+
+    public class RockBand : Band
+    {
+        public override string ToString()
+        {
+            return base.ToString() + " - Rock Band";
+        }
+    }
+
+    public class PopBand : Band
+    {
+        public override string ToString()
+        {
+            return base.ToString() + " - Pop Band";
+        }
+    }
+
+    public class IndieBand : Band
+    {
+        public override string ToString()
+        {
+            return base.ToString() + " - Indie Band";
         }
     }
 }
--- Lab Sheet 1/Q1/MainWindow.xaml.cs
+++ Lab Sheet 1/Q1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,8 @@ namespace Q1
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Band> allBands = new List<Band>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,14 +30,25 @@ namespace Q1
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            Band b1 = new Band() { BandName = "The Foo Fighters", YearFormed = 1994, Members = "Dave Grohl, Nate Mendel, Pat Smear, Taylor Hawkins, Chris Shifflett, Rami Jafee" };
-            Band b2 = new Band() { BandName = "The Beatles", YearFormed = 1960 , Members = "John Lennon, Paul McCartney, George Harrison, Ringo Starr" };
+            RockBand b1 = new RockBand() { BandName = "The Foo Fighters", YearFormed = 1994, Members = "Dave Grohl, Nate Mendel, Pat Smear, Taylor Hawkins, Chris Shifflett, Rami Jafee" };
+            RockBand b2 = new RockBand() { BandName = "The Rolling Stones", YearFormed = 1962, Members = "Mick Jagger, Ian Stewart, Dick Taylor, Bill Wymann, Mick Taylor" };
 
-            Band b3 = new Band() { BandName = "The Rolling Stones", YearFormed = 1962, Members = "Mick Jagger, Ian Stewart, Dick Taylor, Bill Wymann, Mick Taylor" };
-            Band b4 = new Band() { BandName = "Green Day", YearFormed = 1986, Members = "Billie Joe Armstrong, Mike Dirnt, Tre Cool" };
+            PopBand b3 = new PopBand() { BandName = "The Beatles", YearFormed = 1960, Members = "John Lennon, Paul McCartney, George Harrison, Ringo Starr" };
+            PopBand b4 = new PopBand() { BandName = "Green Day", YearFormed = 1986, Members = "Billie Joe Armstrong, Mike Dirnt, Tre Cool" };
 
-            Band b5 = new Band() { BandName = "Artic Monkeys", YearFormed = 2002, Members = "Alex Turner, Matt Heldens, Jamie Cook, Nick O'Malley" };
-            Band b6 = new Band() { BandName = "The Strokes", YearFormed = 1998, Members = "Julien Casablancas, Nick Valensi, Albert Hammond Jr, Nikolai Frature, Fabrizio Moretti" };
+            IndieBand b5 = new IndieBand() { BandName = "Artic Monkeys", YearFormed = 2002, Members = "Alex Turner, Matt Heldens, Jamie Cook, Nick O'Malley" };
+            IndieBand b6 = new IndieBand() { BandName = "The Strokes", YearFormed = 1998, Members = "Julien Casablancas, Nick Valensi, Albert Hammond Jr, Nikolai Frature, Fabrizio Moretti" };
+
+            allBands.Add(b1);
+            allBands.Add(b2);
+            allBands.Add(b3);
+            allBands.Add(b4);
+            allBands.Add(b5);
+            allBands.Add(b6);
+
+            allBands.Sort();
+            
+            lstbxBands.ItemsSource = allBands;
         }
     }
 }
