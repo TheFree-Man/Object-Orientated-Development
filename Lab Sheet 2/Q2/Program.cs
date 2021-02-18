1e55c3d072434087d5099540307fa912a7bfb0af
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2021-02-18 15:07:47 +0000

    Quicksave

---
+++ Lab Sheet 2/Q2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = new DirectoryInfo("C:\\Windows").GetFiles();
+
+            var query = from item in items
+                        where item.length > 10000
+                        orderby item.Length, item.Name
+                        select new MyFileInfo
+                        {
+                            Name = item.Name
+                            Length = item.Length,
+                            CreationTime = item.CreationTime
+                        };
+        }
+    }
+}
---
+++ Lab Sheet 2/Q3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var query = from item in files
+                        where item.length > 10000
+                        orderby item.Length, item.Name
+                        select new
+                        {
+                            Name = item.Name
+                            Length = item.Length
+                            CreationTime = item.CreationTime
+                        }:
+        }
+    }
+}
