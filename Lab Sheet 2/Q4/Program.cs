8f712076d66c75f2c078aa8b32022f9702b74f55
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2021-02-19 21:31:52 +0000

    Quicksave

--- Lab Sheet 2/Q4/Program.cs
+++ Lab Sheet 2/Q4/Program.cs
@@ -8,8 +8,27 @@ namespace Q4
 {
     class Program
     {
+        private static int DoubleIt(int value)
+        {
+            Console.WriteLine("About to double the number " + value.ToString());
+            return value * 2;
+        }
+
         static void Main(string[] args)
         {
+            int[] numbers = { 1, 3, 6, 10, 12, 8 };
+
+            var query = numbers
+                        .Select(n => DoubleIt(n));
+
+            Console.WriteLine("Before the foreach loop");
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
         }
     }
 }
