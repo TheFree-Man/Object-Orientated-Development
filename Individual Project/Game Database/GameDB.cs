1e4a65bb87b87124789604bd80ce6ae94863fab4
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2021-02-21 19:12:17 +0000

    Quicksave

---
+++ Individual Project/Game Database/GameDB.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Database
+{
+    public abstract class GameDB : IComparable
+    {
+        public string GameName { get; set; }
+        public string CreatedBy { get; set; }
+        public int MetacriticRating { get; set; }
+
+        public GameDB(string name, string cretedBy, int metacriticRating)
+        {
+            GameName = name;
+            CreatedBy = cretedBy;
+            MetacriticRating = metacriticRating;
+        }
+
+        public GameDB() : this ("Unknown", "Unknown", 71) { }
+
+        public override string ToString()
+        {
+            return GameName;
+        }
+
+        public int CompareTo(object obj)
+        {
+            GameDB otherGame = obj as GameDB;
+            throw new NotImplementedException();
+        }
+    }
+
+    public class FPS : GameDB
+    {
+        public override string ToString()
+        {
+            return base.ToString() + " - FPS";
+        }
+    }
+
+    public class RPG : GameDB
+    {
+        public override string ToString()
+        {
+            return base.ToString() + " - RPG";
+        }
+    }
+
+    public class RTS : GameDB
+    {
+        public override string ToString()
+        {
+            return base.ToString() + " - RTS";
+        }
+    }
+
+    public class Adventure : GameDB
+    {
+        public override string ToString()
+        {
+            return base.ToString() + " - Adventure";
+        }
+    }
+
+    public class Puzzle : GameDB
+    {
+        public override string ToString()
+        {
+            return base.ToString() + " - Puzzle";
+        }
+    }
+}
--- Individual Project/Game Database/MainWindow.xaml.cs
+++ Individual Project/Game Database/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,52 @@ namespace Game_Database
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<GameDB> allGames = new List<GameDB>();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FPS g1 = new FPS() { GameName = "Half-Life: Alyx", CreatedBy = "Valve Software", MetacriticRating = 93 };
+            FPS g2 = new FPS() { GameName = "F.E.A.R", CreatedBy = "Monolith Productions", MetacriticRating = 88 };
+
+            RPG g3 = new RPG() { GameName = "The Elder Scrolls V: Skyrim", CreatedBy = "Bethesda Game Studios", MetacriticRating = 94 };
+            RPG g4 = new RPG() { GameName = "Disco Elysium", CreatedBy = "ZA/UM", MetacriticRating = 91 };
+
+            RTS g5 = new RTS() { GameName = "Starcraft II", CreatedBy = "Blizzard Entertainment", MetacriticRating = 93 };
+            RTS g6 = new RTS() { GameName = "Ashes of the Singularity: Escalation", CreatedBy = "Stardock Oxide Games", MetacriticRating = 81 };
+
+            Adventure g7 = new Adventure() { GameName = "Psychonauts", CreatedBy = "Double Fine Productions", MetacriticRating = 87 };
+            Adventure g8 = new Adventure() { GameName = "Star Wars Jedi: Fallen Order", CreatedBy = "Respawn Entertainment", MetacriticRating = 81 };
+
+            Puzzle g9 = new Puzzle() { GameName = "Baba is You", CreatedBy = "Hempuli Oy", MetacriticRating = 87 };
+            Puzzle g10 = new Puzzle() { GameName = "Superliminal", CreatedBy = "Pillow Castle Games", MetacriticRating = 74 };
+
+            allGames.Add(g1);
+            allGames.Add(g2);
+            allGames.Add(g3);
+            allGames.Add(g4);
+            allGames.Add(g5);
+            allGames.Add(g6);
+            allGames.Add(g7);
+            allGames.Add(g8);
+            allGames.Add(g9);
+            allGames.Add(g10);
+
+            lstbx_Games.ItemsSource = allGames;
+        }
+
+        private void cbxGenre_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void lstbx_Games_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            GameDB selectedGame = lstbx_Games.SelectedItem as GameDB;
         }
     }
 }
