dcf0594d45cb581efc38d2f0f4073f150f210b42
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2021-02-21 14:16:55 +0000

    Quicksave

---
+++ Lab Sheet 2/Q5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Q5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 5, 3, 6, 10, 12, 8 };
+
+            var query1 = numbers
+                         .OrderByDescending(n => n);
+
+            var query2 = query1
+                         .Where(n => n < 8);
+
+            var query3 = query2
+                .Select(n => DoubleIt(n));
+
+            foreach(var item in query3)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Lab Sheet 2/Q6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Mary", "Joseph", "Michael", "Sarah", "Margaret", "John" };
+
+            var query = from name in names
+                        orderby name ascending
+                        select name;
+                        
+
+            foreach(var name in query)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
