e5c1d81fb848f396a00a6ff0f67426b0a7ba799a
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2021-02-21 15:36:34 +0000

    Quicksave

---
+++ Lab Sheet 2/Q9/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Q9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public class Customer
+        {
+            public string Name;
+            public string City;
+        }
+
+        private static List<Customer> GetCustomers()
+        {
+            Customer c1 = new Customer { Name = "Tom", City = "Dublin" };
+            Customer c2 = new Customer { Name = "Sally", City = "Galway" };
+            Customer c3 = new Customer { Name = "George", City = "Cork" };
+            Customer c4 = new Customer { Name = "Molly", City = "Dublin" };
+            Customer c5 = new Customer { Name = "Joe", City = "Galway" };
+
+            List<Customer> customers = new List<Customer>();
+            customers.Add(c1);
+            customers.Add(c2);
+            customers.Add(c3);
+            customers.Add(c4);
+            customers.Add(c5);
+
+            return customers;
+        }
+    }
+}
