8ccd2958087ff31569a60cb025344999b52d37a1
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2021-04-03 16:39:38 +0100

    Quicksave

--- Lab Sheet 7/Q1/MainWindow.xaml.cs
+++ Lab Sheet 7/Q1/MainWindow.xaml.cs
@@ -29,7 +29,58 @@ namespace Q1
 
         private void Ex1Button_Click(object sender, RoutedEventArgs e)
         {
-            var query = 
+            var query = from c in db.Customers
+                        group c by c.Country into g
+                        orderby g.Count() descending
+                        select new
+                        {
+                            Category = g.Key,
+                            Count = g.Count()
+                        };
+
+            Ex1DGDisplay.ItemsSource = query.ToList();
+        }
+
+        private void Ex2Button_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from c in db.Customers
+                        where c.Country == "Italy"
+                        select new
+                        {
+                            Name = c.ContactName,
+                            Order = c.Orders,
+                            ID = c.CustomerID,
+                            CName = c.CompanyName,
+                            CDemographics = c.CustomerDemographics
+                        };
+
+            Ex2DGDisplay.ItemsSource = query.ToList();
+                        
+        }
+
+        private void Ex3Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Ex4Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Ex5Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Ex6Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Ex7Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
