63d9a7919250d81020e154f837d1d18f9f9be696
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2021-04-17 20:25:36 +0100

    Quicksave

--- Lab Sheet 8/Q1/Team.cs
+++ Lab Sheet 8/Q1/Team.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Q1;
 
 namespace Q1
 {
@@ -11,5 +13,26 @@ namespace Q1
         public int TeamID { get; set; }
         public string TeamName { get; set; }
         public string Location { get; set; }
+
+        public string HomeGround { get; set; }
+        public virtual List<Player> Players { get; set; }
+    }
+
+    public class Player
+    {
+        public int PlayerID { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+
+        public int TeamID { get; set; }
+        public virtual Team Team { get; set; }
+    }
+
+    public class TeamData : DbContext
+    {
+        public TeamData() : base("MyTeamData") { }
+
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Team> Teams { get; set; }
     }
 }
