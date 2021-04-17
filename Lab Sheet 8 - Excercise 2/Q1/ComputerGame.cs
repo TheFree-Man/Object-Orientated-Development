3f9e3562164260bd17cb3ad3770c1dcb0460a6f9
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2021-04-17 20:55:01 +0100

    Initial commit for Lab Sheet 8 - Exercise 2

---
+++ Lab Sheet 8 - Excercise 2/Q1/ComputerGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    class ComputerGame
+    {
+        public string Name { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Genre { get; set; }
+    }
+}
