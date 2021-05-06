8d6596b6f4672f57654f0a52ad7d101bda0f73df
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2021-05-06 20:45:32 +0100

    Quicksave

--- Summer Exam May 2020/Q1/Phone.cs
+++ Summer Exam May 2020/Q1/Phone.cs
@@ -8,6 +8,10 @@ namespace Q1
 {
     class Phone
     {
-        
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string OperatingSystem { get; set; }
+        public string OS_Image { get; set; }
+        public string Phone_Image { get; set; }
     }
 }
