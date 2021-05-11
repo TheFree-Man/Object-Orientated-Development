8d74e72af4af7c2f400dabb2f281b7845314767c
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2021-05-11 10:21:52 +0100

    Added unit test

---
+++ Final Exam May 2021/UnitTestGameApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestGameApplication")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestGameApplication")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4eb40178-e669-4b0c-9e74-a9d79f4d2811")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Final Exam May 2021/UnitTestGameApplication/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using GameApplication;
+
+namespace UnitTestGameApplication
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestPriceDecrease()
+        {
+            Game g1 = new Game("Minecraft", 88, "Test Description", "PC", 20, "");
+            decimal finalPrice = 10;
+
+            g1.DecreasePrice(2);
+
+            Assert.AreEqual(finalPrice, g1.Price);
+        }
+    }
+}
