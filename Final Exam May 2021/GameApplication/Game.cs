8c8fc886bd11e263902f125f90782566798922da
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2021-05-11 10:06:00 +0100

    Created Game class and added required properties

---
+++ Final Exam May 2021/GameApplication/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameApplication
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public int MetariticScore { get; set; }
+        public string Description { get; set; }
+        public string Platform { get; set; }
+        public decimal Price { get; set; }
+        public string Game_Image { get; set; }
+    }
+}
